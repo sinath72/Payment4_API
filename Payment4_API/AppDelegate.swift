d3d00e0badcab4721df8a938e5b4aae8ad059d90
sina taherkhani <registry123456@gmail.com>
2024-07-24 01:18:34 -0700

    [FIXED] BUG FIXED

--- Payment4_API/AppDelegate.swift
+++ Payment4_API/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 class AppDelegate: NSObject,UIApplicationDelegate {
     
     //set this variables with your service's on Payment4 Dashboarrd
-    static var payment4 = Payment4(apiKey: "QAAJJQE2T3Z24NX5YC3KQCMPDNWA0OOU95Y5XVGI8CHL6MI1IU375G0UZXL2", callbackUrl: "https://taherkhani.shop/", webhoockUrl: "https://taherkhani.shop/wh")
+    static var payment4 = Payment4(apiKey: "", callbackUrl: "", webhoockUrl: "")
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         return true
